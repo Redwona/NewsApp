bbfa9214ffe2722df9607c4bf226003d91f8df9a
Redwona <islam1707113@stud.kuet.ac.bd>
2021-11-30 10:12:35 -0800

    new java class added

---
+++ app/src/main/java/com/project/newsapp/vasha.java
@@ -0,0 +1,17 @@
+package com.project.newsapp;
+
+public class vasha {
+    int v;
+
+    public vasha(int v) {
+        this.v = v;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+}
